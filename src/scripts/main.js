fb29eb5fb92f1c7e3e407dad18d000039dd71e30
John <cyhsu.john@gmail.com>
2021-04-28 14:43:14 +0800

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-// 實作寫在這裡！
+
+const addToCart = button => {
+  button.addEventListener('click', (e) => {
+    const card = e.currentTarget.parentElement.parentElement
+    const price = parseFloat(card.querySelector('.price')
+                      .textContent
+                      .replace('$',''))
+    const title = card.querySelector('.card-title').textContent
+  })}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  const catButton = document.querySelectorAll('.card .btn')
+  catButton.forEach(addToCart)
+})
